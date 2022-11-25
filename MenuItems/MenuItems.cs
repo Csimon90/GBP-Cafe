e46169724b0b80a1dd7f09468ebae380631fa1a9
Caitlin Simon <csimondesign@gmail.com>
2022-11-25 11:45:03 -0500

    push

---
+++ MenuItems/MenuItems.cs
@@ -0,0 +1,61 @@
+﻿public class NewBaseType
+{
+    public string IngredientList { get; set; }
+}
+
+public class MenuItems : NewBaseType
+{
+
+    public MenuItems()
+    {        
+
+    }
+
+    public MenuItems
+    (int itemNumber, string itemName, string ingredientList, string price, MenuItems menuItems)
+
+    {
+        ItemNumber = itemNumber;
+        ItemName = itemName;
+        IngredientList = ingredientList;
+        price = price;
+
+    }
+        public MenuItems(int itemNumber, string description, int price,  MenuItems menuItems) 
+        {
+            this.ItemNumber = itemNumber;
+            this.Description = description;
+            this.price = price;   
+        }
+        public int ItemNumber {get; set;}
+public string ItemName {get; set;}
+public string Description {get; set;}
+public int price {get; set;}
+
+public string Order
+{
+    get
+    {
+        return $"{ItemNumber}{ItemName}{price}";
+    }
+}
+
+public MenuItems menuItems {get; set;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
